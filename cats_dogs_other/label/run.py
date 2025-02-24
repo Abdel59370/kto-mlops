cf229cfc0ae437388ca562f000a8aa157765a341
Abdel59370 <133366779+Abdel59370@users.noreply.github.com>
2025-02-24 12:13:38 +0000

    feat: cat dog recognizer ml

--- cats_dogs_other/label/run.py
+++ cats_dogs_other/label/run.py
@@ -1,4 +1,14 @@
+import os
+from extraction import extract_images
 
 if __name__ == "__main__":
-    # Faire le code ici
-    print('')
+    # Préparation du dataset
+    raw_folder = "cats_dogs_other/label/dataset/_raw"
+    postprocess_folder = "cats_dogs_other/label/dataset/extract"
+
+    if not os.path.isdir(postprocess_folder) or not os.listdir(postprocess_folder):
+        res = extract_images(raw_folder, postprocess_folder)
+        print(f"Nombre de fichiers PDF en entrée : {res.number_files_input}")
+        print(f"Nombre d'images extraites : {res.number_images_output}")
+    else:
+        print("Les images sont déjà extraites.")
