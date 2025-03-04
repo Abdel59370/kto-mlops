054b01495f384b2da598037e61ac0fc342fdc224
Abdel59370 <133366779+Abdel59370@users.noreply.github.com>
2025-03-04 08:27:01 +0000

    Correction et reconstruction du package kto-keras-inference : - Fix du setup.py (gestion des dépendances et paths) - Rebuild du fichier .whl - Vérification et mise à jour de requirements.txt - Organisation des fichiers dans le repo - Nettoyage et correction des erreurs de dépendances

--- init_packages.sh
+++ init_packages.sh
@@ -7,5 +7,4 @@ cd packages/inference/
 python -m build --sdist --wheel
 cd dist
 cp *.whl ../../../cats_dogs_other/train/packages
-cp *.whl ../../../cats_dogs_other/api/packages
 cd ../../../
--- packages/inference/setup.py
+++ packages/inference/setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
+# Lire requirements.txt en filtrant les lignes vides et les commentaires
 with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+    requirements = [line.strip() for line in f.readlines() if line.strip() and not line.startswith("#")]
 
-setup(name = 'kto-keras-inference',
-      version = '0.0.1',
-      install_requires=requirements
-     )
+setup(
+    name='kto-keras-inference',
+    version='0.0.1',
+    install_requires=requirements
+)
